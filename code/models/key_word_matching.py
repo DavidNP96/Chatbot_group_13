c9657982c109bc0316e2a21ebf6a1346b1c88b51
David417 <david@imagin.studio>
2021-09-20 09:55:33 +0200

    pull

--- code/models/key_word_matching.py
+++ code/models/key_word_matching.py
@@ -1,3 +1,5 @@
+import test
+import data_class
 import numpy as np
 import random
 import sklearn.metrics as skm
@@ -5,34 +7,30 @@ from operator import itemgetter
 from collections import Counter
 import sys
 sys.path.append("../data")
-import data_class
-import test
-
-
 
 
 def main():
-    ### classify sentence based on keywords found in sentence
+    # classify sentence based on keywords found in sentence
     print("keyword matching metrics:")
-    labels = ['inform', 'request', 'thankyou', 'reqalts', 'null', 'affirm', 'bye', 
-    'confirm', 'hello', 'negate', 'deny', 'repeat', 'ack', 'restart', 'reqmore']
+    labels = ['inform', 'request', 'thankyou', 'reqalts', 'null', 'affirm', 'bye',
+              'confirm', 'hello', 'negate', 'deny', 'repeat', 'ack', 'restart', 'reqmore']
 
-    keywords = {"inform":["know", "food", "restaurant", "town", "part"], 
-                    "request": ["number", "phone", "address", "whats", "code", "post"], 
-                    "thankyou": ["thanks", "appreciate", "thank"], "reqalts":["else", "anything"], 
-                    "null": ["ah", "uh", "ugh", "oh", "noise", "sil", "unintelligible"], 
-                    "affirm": ["yes", "true", "correct","affirmative", "uh-huh", "agree", "acknowledge","concede", "right", "yea", "yeah"], 
-                    "bye": ["goodbye", "bye", "cya", "farewell", "laters"],
-                    "confirm": ["serve","priced","center"], 
-                    "hello": ["hi", "hey", "hello", "greetings"], 
-                    "negate": ["not","none"], 
-                    "deny": ["dont", "wont","no", "negative", "false","disagree", "reject", "wrong", "want", "incorrect"],
-                    "repeat":["repeat", "recur", "echo", "back"], 
-                    "ack":["okay", "alright", "ok", "kay", "um"], 
-                    "restart":["start", "reset", "restart"], 
-                    "reqmore":["more","other"]}
+    keywords = {"inform": ["know", "food", "restaurant", "town", "part"],
+                "request": ["number", "phone", "address", "whats", "code", "post"],
+                "thankyou": ["thanks", "appreciate", "thank"], "reqalts": ["else", "anything"],
+                "null": ["ah", "uh", "ugh", "oh", "noise", "sil", "unintelligible"],
+                "affirm": ["yes", "true", "correct", "affirmative", "uh-huh", "agree", "acknowledge", "concede", "right", "yea", "yeah"],
+                "bye": ["goodbye", "bye", "cya", "farewell", "laters"],
+                "confirm": ["serve", "priced", "center"],
+                "hello": ["hi", "hey", "hello", "greetings"],
+                "negate": ["not", "none"],
+                "deny": ["dont", "wont", "no", "negative", "false", "disagree", "reject", "wrong", "want", "incorrect"],
+                "repeat": ["repeat", "recur", "echo", "back"],
+                "ack": ["okay", "alright", "ok", "kay", "um"],
+                "restart": ["start", "reset", "restart"],
+                "reqmore": ["more", "other"]}
 
-    # import data                
+    # import data
     data = data_class.Data("../../data/dialog_acts.dat")
     train_sents = data.train_sents
     train_labels = data.train_labels
@@ -42,20 +40,22 @@ def main():
         labels_counter = Counter()
 
         for word in sentence.split(' '):
-            
+
             for label, keyword in keywords.items():
                 if word in keyword:
                     labels_counter[label] += 1
 
-        # select the most probable one            
+        # select the most probable one
         mx = labels_counter.most_common(1)
 
         # check if there is a tie between most likely labels
-        maxes = [ele[0] for ele in labels_counter.items() if ele[1] >= mx[0][1]]
+        maxes = [ele[0]
+                 for ele in labels_counter.items() if ele[1] >= mx[0][1]]
 
         if len(maxes) == 1:
             train_pred.append(maxes[0])
-        elif len(maxes) == 0: # if no keywords are found select the most probable class based on distribution
+        # if no keywords are found select the most probable class based on distribution
+        elif len(maxes) == 0:
             train_pred.append("inform")
         else:
             # check which of the multiple classes is most likely based on prior
@@ -65,17 +65,13 @@ def main():
                     if max == label and filled == False:
                         train_pred.append(label)
                         filled = True
-    
-    
-   
 
     test.get_metrics(train_pred, train_labels)
-    
+
     return
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     x_test = data.test_sents
     y_test = data.test_labels
     # keyword_matching(labels, keywords, train_sents, train_labels)
