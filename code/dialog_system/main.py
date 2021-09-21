668d7e51d0a2889326635aa125efbc3696089228
SimonvdFliert <Simonvdfliert@hotmail.com>
2021-09-21 15:27:22 +0200

    Added a Restaurant class
    Added a restaurant class where an instance has the total dataset in a csv file converted into a pd dataframe. Then the dataframe can be filtered based on area, foodtype and price. Has been tested

--- code/dialog_system/main.py
+++ code/dialog_system/main.py
@@ -1,19 +1,26 @@
 ### the main file to run the dialog system
 
 # import models
-
+import pandas as pd
 
 
 def main():
     # initial interface for the dialog system
     match = False
 
-    ds = Dialog_system()
+    #ds = Dialog_system()
 
-    while match == False:
-        customer_input = input("").lower()
-        response, match = ds.updated_customer_input(customer_input)
-        print(response)
+    #while match == False:
+    #    customer_input = input("").lower()
+    #    response, match = ds.updated_customer_input(customer_input)
+    #    print(response)
+
+    rs = RestaurantInfo()
+    print(rs.data)
+    print("\n\n\n\n ===================== \n\n\n")
+    preference = ["centre","european","moderate"]
+    filtered_data = rs.filter_info(preference)
+    print(filtered_data)
 
 class Dialog_system:
 
@@ -72,6 +79,51 @@ class Dialog_state:
         # use imported model to predict class
         model = "" # import model
         self.dialog_state = model.predict(customer_input)
+
+
+class RestaurantInfo:
+
+    def __init__(self):   
+        self.data = self.load_data()
+
+    #Load function
+    ###Load in dataset and change it in a pandas setup
+    def load_data(self):
+        restaurants_info = pd.read_csv("C:\\Users\\Simon\\Documents\\Master_Code\\Chatbot_group_13\\data\\restaurant_info.csv")
+        return restaurants_info
+
+    ##Filter function(pd dataframe, array/list of preferences)
+    def filter_info(self, filter_preferences: list):
+        
+        #unpack array and place array elements into variables
+        area = filter_preferences[0]
+        food_type = filter_preferences[1]
+        price = filter_preferences[2]
+       
+        ##check if variables exist, and if so, filter the dataframe on it
+        if (area != "") & (food_type != "") & (price != ""):
+            filtered_restaurant_info = self.data[((self.data["area"]== area) & (self.data["food"]==food_type)) & (self.data["pricerange"]==price)]
+
+        elif (area != "") & (food_type != ""):
+            filtered_restaurant_info = self.data[((self.data["area"]== area) & (self.data["food"]==food_type))]
+
+        elif (area != "") &  (price != ""):
+            filtered_restaurant_info = self.data[((self.data["area"]== area) & (self.data["pricerange"]==price))]
+
+        elif (food_type != "") & (price != ""):
+            filtered_restaurant_info = self.data[(((self.data["food"]==food_type)) & (self.data["pricerange"]==price))]
+
+        elif (area != ""):
+            filtered_restaurant_info = self.data["area"]== area
+        
+        elif (price != ""):
+            filtered_restaurant_info = self.data["pricerange"]== price
+        
+        elif (food_type != ""):
+            filtered_restaurant_info = self.data["food"]== food_type
+
+        return filtered_restaurant_info
+
 
 if __name__=="__main__":
     main()
